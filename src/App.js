336e61eced0d7674a3cfb65f6ae86aba352cedd7
jreyesgfi <jorge1998reyes1998@gmail.com>
2021-11-13 14:49:03 +0100

    separación del codigo ite peliculas

--- src/App.js
+++ src/App.js
@@ -6,12 +6,93 @@ import PageWrapper from './PageWrapper';
 
 
 function App() {
-  return(
+  {/*
+          <div className="movie-item-style-2">
+          <img src="images/uploads/mv1.jpg" alt="" />
+          <div className="mv-item-infor">
+              <h6><a href="moviesingle.html">oblivion <span>(2012)</span></a></h6>
+              <p className="rate"><i className="ion-android-star"></i><span>8.1</span> /10</p>
+              <p className="describe">Earth's mightiest heroes must come together and learn to fight as a team if they are to stop the mischievous Loki and his alien army from enslaving humanity...</p>
+              <p className="run-time"> Run Time: 2h21’    .     <span>MMPA: PG-13 </span>    .     <span>Release: 1 May 2015</span></p>
+              <p>Director: <a href="#">Joss Whedon</a></p>
+              <p>Stars: <a href="#">Robert Downey Jr.,</a> <a href="#">Chris Evans,</a> <a href="#">  Chris Hemsworth</a></p>
+          </div>
+      </div>
+      <div className="movie-item-style-2">
+          <img src="images/uploads/mv2.jpg" alt="" />
+          <div className="mv-item-infor">
+              <h6><a href="moviesingle.html">into the wild <span>(2014)</span></a></h6>
+              <p className="rate"><i className="ion-android-star"></i><span>7.8</span> /10</p>
+              <p className="describe">As Steve Rogers struggles to embrace his role in the modern world, he teams up with a fellow Avenger and S.H.I.E.L.D agent, Black Widow, to battle a new threat...</p>
+              <p className="run-time"> Run Time: 2h21’    .     <span>MMPA: PG-13 </span>    .     <span>Release: 1 May 2015</span></p>
+              <p>Director: <a href="#">Anthony Russo,</a><a href="#">Joe Russo</a></p>
+              <p>Stars: <a href="#">Chris Evans,</a> <a href="#">Samuel L. Jackson,</a> <a href="#">  Scarlett Johansson</a></p>
+          </div>
+      </div>
+      <div className="movie-item-style-2">
+          <img src="images/uploads/mv3.jpg" alt="" />
+          <div className="mv-item-infor">
+              <h6><a href="moviesingle.html">blade runner  <span>(2015)</span></a></h6>
+              <p className="rate"><i className="ion-android-star"></i><span>7.3</span> /10</p>
+              <p className="describe">Armed with a super-suit with the astonishing ability to shrink in scale but increase in strength, cat burglar Scott Lang must embrace his inner hero and help...</p>
+              <p className="run-time"> Run Time: 2h21’    .     <span>MMPA: PG-13 </span>    .     <span>Release: 1 May 2015</span></p>
+              <p>Director: <a href="#">Peyton Reed</a></p>
+              <p>Stars: <a href="#">Paul Rudd,</a> <a href="#"> Michael Douglas</a></p>
+          </div>
+      </div>
+      <div className="movie-item-style-2">
+          <img src="images/uploads/mv4.jpg" alt="" />
+          <div className="mv-item-infor">
+              <h6><a href="moviesingle.html">Mulholland pride<span> (2013)  </span></a></h6>
+              <p className="rate"><i className="ion-android-star"></i><span>7.2</span> /10</p>
+              <p className="describe">When Tony Stark's world is torn apart by a formidable terrorist called the Mandarin, he starts an odyssey of rebuilding and retribution.</p>
+              <p className="run-time"> Run Time: 2h21’    .     <span>MMPA: PG-13 </span>    .     <span>Release: 1 May 2015</span></p>
+              <p>Director: <a href="#">Shane Black</a></p>
+              <p>Stars: <a href="#">Robert Downey Jr., </a> <a href="#">  Guy Pearce,</a><a href="#">Don Cheadle</a></p>
+          </div>
+      </div>
+    */}
+  let peliculas = [
+    {
+      titulo:"oblivion",
+      calificacion:"8.1",
+      director:"Joss Whedon",
+      actores:"Robert Downey Jr., Chris Evans, Chris Hemsworth",
+      fecha:"1 May 2015",
+      duracion:"2h y 21min",
+      img:"images/uploads/mv1.jpg",
+      descripcion:"Earth's mightiest heroes must come together and learn to fight as a team if they are to stop the mischievous Loki and his alien army from enslaving humanity...",
+    },
+    {
+      titulo:"Mulholland pride",
+      calificacion:"7.2",
+      director:"Shane Black",
+      actores:"Robert Downey Jr., Guy Pearce, Don Cheadle",
+      fecha:"1 May 2015",
+      duracion:"2h y 21min",
+      img:"images/uploads/mv4.jpg",
+      descripcion:"When Tony Stark's world is torn apart by a formidable terrorist called the Mandarin, he starts an odyssey of rebuilding and retribution.",
+    },
+    {
+      titulo:"skyfall: evil of boss",
+      calificacion:"7.0",
+      director:"Alan Taylor",
+      actores:"Chris Hemsworth, Natalie Portman,Tom Hiddleston",
+      fecha:"1 May 2015",
+      duracion:"2h y 21min",
+      img:"images/uploads/mv5.jpg",
+      descripcion:"When Tony Stark's world is torn apart by a formidable terrorist called the Mandarin, he starts an odyssey of rebuilding and retribution.",
+    }
+  ]
+  return (
     <PageWrapper>
-      <Pelicula titulo="oblivion" calificacion="8.1" img="images/uploads/mv1.jpg"
-        director="Joss Whedon" actores="Robert Downey Jr., Chris Evans, Chris Hemsworth" fecha="1 May 2015" duracion="2h21'">
-        Earth's mightiest heroes must come together and learn to fight as a team if they are to stop the mischievous Loki and his alien army from enslaving humanity...
+
+      {peliculas.map(pelicula=>{
+        return <Pelicula titulo= {pelicula.titulo} calificacion={pelicula.calificacion} img={pelicula.img}
+        director={pelicula.director} actores={pelicula.actores} fecha={pelicula.fecha} duracion={pelicula.duracion}>
+        {pelicula.descripcion}
       </Pelicula>
+      })}
     </PageWrapper>
   )
 }
