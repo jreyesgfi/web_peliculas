fc8d2415d289ee5b8743444dbc32acf7f6090255
jreyesgfi <jorge1998reyes1998@gmail.com>
2021-11-16 03:24:49 +0100

    todo preparado para el segundo hook

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import logo from './logo.svg';
 import './App.css';
 import Pelicula from './Pelicula';
 import PageWrapper from './PageWrapper';
-import peliculasJson from './peliculas.json'
 import { useState } from 'react';
 import Paginacion from './Paginacion';
 
@@ -10,15 +9,16 @@ import Paginacion from './Paginacion';
 
 function App() {
   const [paginaActual, setPaginaActual] = useState(1);
+  const [peliculas, setPeliculas] = useState([]);
 
 
   //Solicitamos las películas de un servidor
-  function buscarPeliculas(){
+  function buscarPeliculas() {
     let url = 'https://lucasmoy.dev/data/react/peliculas.json'
     try {
       fetch(url).then(response => {
         if (response.status === 200) {
-          response.json().then(data=> setMovies(data));
+          response.json().then(data => setPeliculas(data));
           return null;
         }
       });
@@ -28,16 +28,9 @@ function App() {
     }
   };
   buscarPeliculas();
-  function setMovies(data) {
-    console.log(data);
-  }
-
-
-
-  let peliculas = peliculasJson;
 
   //Mostramos un número limitado de películas por página
-  const pelPorPag = 2;
+  const pelPorPag = 4;
   let cargarPeliculas = () =>
     peliculas.slice(
       (paginaActual - 1) * pelPorPag,
@@ -46,8 +39,7 @@ function App() {
 
 
   //Número de páginas necesarias
-  const getTotalPaginas = () => (Math.ceil(peliculasJson.length / pelPorPag))
-
+  const getTotalPaginas = () => (Math.ceil(peliculas.length / pelPorPag));
 
   return (
     <PageWrapper>
